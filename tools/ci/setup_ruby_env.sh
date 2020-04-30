ee1ef4cafc87c0154f85c2452a07a61f2105d56e
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-04-30 10:07:09 +0530

    Add flag for sassc installation on ppc64le
    The build fails while installing (or building) sassc gem if the --disable-march-tune-native flag is not set for ppc64le.
    
    More context : #19520

--- tools/ci/setup_ruby_env.sh
+++ tools/ci/setup_ruby_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./tools/ci/setup_ruby_environment.rb
+bundle config --local build.sassc --disable-march-tune-native
 export BUNDLE_WITHOUT=development
 export BUNDLE_GEMFILE=${PWD}/Gemfile
